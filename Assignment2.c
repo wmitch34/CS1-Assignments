9ddc9002f1d289790dc5551bf8ab6131b8289d23
Will Mitchell <wmitch34@gmail.com>
2021-02-03 20:50:59 -0500

    More testing git commands

--- Assignment2.c
+++ Assignment2.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 //learning how to use git hub tbh
+//Messing around with github commands. ignore
 int checkResponse(char* response);
 int generateGuess(int respNum);
 
