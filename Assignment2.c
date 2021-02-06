ccfefd31b55edaea42bd96dcf2901efa6cf01bf8
Will Mitchell <wmitch34@gmail.com>
2021-02-05 22:57:03 -0500

    First working version of the guessing game. no error messages or strenuous testing done

--- Assignment2.c
+++ Assignment2.c
@@ -3,62 +3,66 @@
 #include <string.h>
 #include <math.h>
 
-void generateGuess(int, int, int);
-int askTheCat(int);
+//Will Mitchell feb 5, 2021
+//pick a number between 1 and n
+//enter n in terminal
+//program will guess numbers
+//you reply warmer "w" colder "c"
+//of yes "y"
+
+void generateGuess(int, int*, int*);
+int askTheCat();
+int mid(int, int);
 
 int main(){
-	char response[50];
-	int max, guess, responseInt;
-	//scan for max
-	scanf("%d", &max);
-	printf("%d\n", max/2);
-	responseInt = askTheCat(max/2);
-	generateGuess(1, max, responseInt);
+	int guess, answer;
+	int low = 1;
+	int high;
+	
+
+	scanf("%d", &high);
+	printf("%d\n",mid(low, high));
+	answer = askTheCat();
+	printf("%d\n", 1 +mid(low, high));
+	answer = askTheCat();
+
+	while (answer != 1){
+		if(answer == 3){
+			low = (mid(low, high));
+			high = high;
+			printf ("range is %d - %d\n", low, high);
+			guess = mid(low, high);
+			printf("%d\n", guess);
+			answer = askTheCat();
+			if (answer != 1){
+				guess = guess + 1;
+				printf("%d\n", guess);
+				answer = askTheCat();
+			}
+		}
+		if(answer == 4){
+			low = low;
+			high = mid(low, high);
+			printf ("range is %d - %d\n", low, high);
+			guess = mid(low, high);
+			printf("%d\n", guess);
+			answer = askTheCat();
+			if (answer != 1){
+				guess = guess + 1;
+				printf("%d\n", guess);
+				answer = askTheCat();
+			}
+		}
+	}
 	return 0;
 }
 
-void generateGuess(int low, int high, int response){
-	int answer;
-	int guess;
-
-	if(response == 1){
-		return;
-	}
-	if(response == 2){
-		guess = ((high/2)+1);
-		printf("%d\n", guess);
-		answer = askTheCat(guess);
-		generateGuess(low, high, answer);
-
-	}
-	if(response == 3){
-		low = (((high-low)/2)+low);
-		high = high;
-		guess = (((high-low)/2)+low);
-		printf("%d\n", guess);
-		answer = askTheCat(guess);
-		generateGuess(low, high, answer);
-
-	}
-	if(response == 4){
-		low = low;
-		high = (((high-low)/2)+low);
-		guess = (((high-low)/2)+low);
-		printf("%d\n", guess);
-		answer = askTheCat(guess);
-		generateGuess(low, high, answer);
-	}
-
-	
+int mid(int low, int high){
+	int mid = ((high - low)/2) + low;
+	return mid;
 }
-//Takes in a computer generated guess
-//returns a number based on user input string
-// "Yes!!!" 	 = 1
-// "No." 		 = 2
-// "No. Warmer." = 3
-// "No. Colder." = 4
-// any invalid entry returns 5
-int askTheCat(int guess){
+
+int askTheCat(){
 	char response[50];
 	scanf(" %[^\n]s", response);
 	if (strcmp(response, "y") == 0){
