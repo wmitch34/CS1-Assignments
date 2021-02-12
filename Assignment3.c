bcd1a8d384704f70dd1169103895d8c010dc7796
Will Mitchell <wmitch34@gmail.com>
2021-02-11 19:01:50 -0500

    priming recursive function and testing with factorials

--- Assignment3.c
+++ Assignment3.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+int recursiveFact(int);
+
 int main(){
-	
+	int n;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	printf("%d! is equal to %li\n", n, recursiveFact(n));
 	return 0;
+}
+
+int recursiveFact(int n){
+	if(n ==1){
+		return 1;
+	}
+	else{
+		return n * recursiveFact(n-1);
+	}
 }
