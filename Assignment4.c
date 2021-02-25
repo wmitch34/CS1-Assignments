e4cc55bb185538c1c7f3067fe0e764a4ef9780a1
Will Mitchell <wmitch34@gmail.com>
2021-02-24 22:33:30 -0500

    wireframing for ass4

--- Assignment4.c
+++ Assignment4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//Given a list of individuals and how much they paid followed by a value ratio for the 
+//currency,sort the individuals from GREATEST to LEAST in terms of paid value.
 
 int main(){
 	printf("Hello tylo\n");
