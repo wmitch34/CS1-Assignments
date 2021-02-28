6919048230f7ade9e3bca5fc6c3148052fa08e8d
Will Mitchell <wmitch34@gmail.com>
2021-02-27 22:53:37 -0500

    completed struct and unit generator function

--- Assignment4.c
+++ Assignment4.c
@@ -5,30 +5,47 @@
 typedef struct customer{
     int bills;
     int tokens;
+    int units;
     char name[BUFFER+1];
 }customer;
 
+long long int convertToUnits(int tokens, int bills, int tokenValue, int billValue);
+
 int main(){
-    customer * arrList;
-    int bills, tokens, count;
+    customer * customerList;
+    int bills, tokens, count, tokenValue, billValue;
     char name[BUFFER+1];
 
     scanf("%d", &count);
-    arrList = (customer*) calloc(count, sizeof(customer));
+    customerList = (customer*) calloc(count, sizeof(customer));
 
     for(int i = 0; i < count; i++){
-        scanf("%s", arrList[i].name);
-        scanf("%d", &arrList[i].tokens);
-        scanf("%d", &arrList[i].bills);
+        scanf("%s", customerList[i].name);
+        scanf("%d", &customerList[i].tokens);
+        scanf("%d", &customerList[i].bills);
     }
+
+    scanf("%d", &tokenValue);
+    scanf("%d", &billValue);
+
     for(int i = 0; i < count; i++){
-        printf("%s", arrList[i].name);
-        printf("%d", arrList[i].tokens);
-        printf("%d", arrList[i].bills);
+        customerList[i].units = convertToUnits(customerList[i].tokens, customerList[i].bills, tokenValue, billValue);
     }
 
+    for(int i = 0; i < count; i++){
+        printf("%s ", customerList[i].name);
+        printf("%d ", customerList[i].tokens);
+        printf("%d ", customerList[i].bills);
+        printf("%d \n", customerList[i].units);
+    }
 
-    free(arrList);
+    free(customerList);
 
     return 0;
+}
+
+long long int convertToUnits(int tokens, int bills, int tokenValue, int billValue){
+    long int units;
+    units = (tokens * billValue) + (bills * tokenValue);
+    return units;
 }
