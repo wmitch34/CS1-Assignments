b434fb327f7ab0878e0bda8da241e2be7078249a
Will Mitchell <wmitch34@gmail.com>
2021-02-27 22:10:25 -0500

    arraylist setup for customer list

--- Assignment4.c
+++ Assignment4.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-//Given a list of individuals and how much they paid followed by a value ratio for the 
-//currency,sort the individuals from GREATEST to LEAST in terms of paid value.
+#define BUFFER 20
+
+typedef struct customer{
+    int bills;
+    int tokens;
+    char name[BUFFER+1];
+}customer;
 
 int main(){
-	printf("Hello tylo\n");
-	return 0;
+    customer * arrList;
+    int bills, tokens, count;
+    char name[BUFFER+1];
+
+    scanf("%d", &count);
+    arrList = (customer*) calloc(count, sizeof(customer));
+
+    for(int i = 0; i < count; i++){
+        scanf("%s", arrList[i].name);
+        scanf("%d", &arrList[i].tokens);
+        scanf("%d", &arrList[i].bills);
+    }
+    for(int i = 0; i < count; i++){
+        printf("%s", arrList[i].name);
+        printf("%d", arrList[i].tokens);
+        printf("%d", arrList[i].bills);
+    }
+
+
+    free(arrList);
+
+    return 0;
 }
