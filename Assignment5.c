9d88ca1e255f10fcab9fa3916d2754c7bd822194
Will Mitchell <wmitch34@gmail.com>
2021-03-10 21:32:34 -0500

    wireframe

--- Assignment5.c
+++ Assignment5.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BUFFER 20
+
+typedef struct gamer{
+	int tokens;
+	char name[BUFFER + 1];
+	struct gamer * pointer;
+}gamer;
+
+gamer * addGamer(){
+	gamer * newGamer;
+	newGamer = calloc(1, sizeof(gamer));
+	return newGamer;
+}
+
+gamer * removeGamer(){
+
+}
 
 int main(){
-	printf("Hello World\n");
+	struct gamer * head = NULL;
+	int userInput;
+	do{
+		scanf("%d", &userInput);
+		switch(userInput){
+			case 0:
+			break;
+
+			case 1:
+			//input gamer
+			//add gamer
+			break;
+
+			case 2:
+			//removeGamer
+			break;
+
+		}
+	}while(userInput != 0);
+	
 	return 0;
 }
