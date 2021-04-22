2c75ab87929655738b2676f709fc552347c2ef35
Will Mitchell <wmitch34@gmail.com>
2021-04-22 11:38:49 -0400

    setup for grid, assignment7

--- Assignment7.c
+++ Assignment7.c
@@ -2,5 +2,33 @@
 #include <stdlib.h>
 
 int main(){
+	int row, col, numGroupsRows, numGroupsCols;
+	int ** grid;
+	int * rowGroupArr, * colGroupArr;
+
+	scanf("%i", &row);
+	scanf("%i", &col);
+
+	grid = (int **) calloc(row, sizeof(int*));
+	for (int i = 0; i < row; i++){
+		grid[i] = (int*) calloc(col, sizeof(int));
+	}
+
+	for(int i = 0; i < row; i++){
+		scanf("%i", &numGroupsRows);
+		rowGroupArr = (int *)calloc(numGroupsRows, sizeof(int));
+		for(int i = 0; i < numGroupsRows; i++){
+			scanf("%i",&rowGroupArr[i]);
+		}
+	}
+
+	for(int i = 0; i < col; i++){
+		scanf("%i", &numGroupsCols);
+		colGroupArr = (int*)calloc(numGroupsCols, sizeof(int));
+		for(int i = 0; i < numGroupsCols; i++){
+			scanf("%i",&colGroupArr[i]);
+		}
+	}
+
 	return 0;
 }
